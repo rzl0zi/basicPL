d82b3fe5e3bd47590cbaf57fb74cff58eb6d85dc
rzl0zi <133296202+rzl0zi@users.noreply.github.com>
2023-05-25 02:00:36 +0900

    Update and rename baekjoon to 3003.cpp

---
+++ 3003.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int K, Q, L, B, N, P;
+	int k, q, l, b, n, p;
+	K = 1;
+	Q = 1;
+	L = 2;
+	B = 2;
+	N = 2;
+	P = 8;
+	cin >> k >> q >> l >> b >> n >> p;
+	cout << K - k <<" " << Q - q << " " << L - l << " " << B - b << " " << N - n <<" " << P - p;
+
+
+
+		
+	
+	
+
+	return 0;
+}
